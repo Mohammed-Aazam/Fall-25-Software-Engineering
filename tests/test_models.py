bb7f215bd2774dc148501365605192a32dd4a060
Mohammed-Aazam <32879466+Mohammed-Aazam@users.noreply.github.com>
2026-03-01 23:32:59 -0500

    Create test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,15 @@
+import sys  # <-- LINT ERROR (F401): 'sys' imported but unused.
+
+# We'll import User later when we write real tests
+# from moj.models import User
+
+
+# LINT ERROR (E302): expected 2 blank lines, found 1
+def test_new_user_stub():
+    """
+    This is a stub for testing the User model.
+    It is intentionally left with lint errors.
+    """
+    # TODO: This test needs to be written!
+    new_user = "test" # <-- LINT ERROR (F841): local variable 'new_user' is assigned to but never
+    assert 1 == 1
